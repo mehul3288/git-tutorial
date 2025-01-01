944d42e063a04d84c996934903ae513f7c9141ae
mehul3288 <mehulr286@gmail.com>
2025-01-01 20:01:45 +0530

    Version 3

--- change.js
+++ change.js
@@ -1,2 +1,3 @@
 console.log("change");
 console.log("change version 2");
+console.log("change version 3");
--- config.js
+++ config.js
@@ -2,3 +2,4 @@ console.log("mehul");
 console.log("mehul1");
 console.log("mehul3");
 console.log("Version 2");
+console.log("Version 3");
